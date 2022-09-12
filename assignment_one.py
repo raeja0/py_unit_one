636f92e4a634479f8b5fa4cd5555244a38ee2902
raeja0 <raeja@ssfs.org>
2022-09-12 14:56:54 -0400

    Update assignment_one.py

--- assignment_one.py
+++ assignment_one.py
@@ -1,11 +1,28 @@
 # remember to use comments!
 import turtle
 
+turtle.speed(100)
+def makealargesquare():
+    for x in range(4):
+        turtle.forward(200)
+        turtle.left(90)
 
+def makeamediumsquare():
+    for x in range(4):
+        turtle.forward(100)
+        turtle.left(90)
 
-for x in range(3):
-    turtle.forward(200)
-    turtle.left(120)
+def makeasmallsquare():
+    for x in range(4):
+        turtle.forward(50)
+        turtle.left(90)
 
+for y in range (40):
+    makealargesquare()
+    turtle.left(3)
+    makeamediumsquare()
+    turtle.left(3)
+    makeasmallsquare()
+    turtle.left(3)
 
 turtle.exitonclick()
