5329791112b9c2e6e40dd7fe3936698405be3f73
raeja0 <raeja@ssfs.org>
2022-09-14 08:53:32 -0400

    house is now colored in

--- drawing_houses.py
+++ drawing_houses.py
@@ -1,11 +1,27 @@
 import turtle
 
-for x in range(4):
-    turtle.forward(200)
-    turtle.right(90)
+turtle.ht()
 
-for y in range(3):
-    turtle.forward(200)
-    turtle.left(120)
+from turtle import *
+color('red')
+begin_fill()
+while True:
+    for x in range(4):
+        turtle.forward(200)
+        turtle.right(90)
+    if abs(pos()) < 1:
+        break
+end_fill()
+
+color('blue')
+begin_fill()
+while True:
+    for y in range(3):
+        turtle.forward(200)
+        turtle.left(120)
+    if abs(pos()) < 1:
+        break
+end_fill()
+done()
 
 turtle.exitonclick()
