28378a99442d4bd1c4bea4243699e7dd63b0a6f4
raeja0 <raeja@ssfs.org>
2022-09-14 09:00:31 -0400

    Defined house but not made town

--- drawing_houses.py
+++ drawing_houses.py
@@ -1,27 +1,27 @@
 import turtle
 
-turtle.ht()
-
 from turtle import *
-color('red')
-begin_fill()
-while True:
-    for x in range(4):
-        turtle.forward(200)
-        turtle.right(90)
-    if abs(pos()) < 1:
-        break
-end_fill()
+def draw_house():
+    color('red')
+    begin_fill()
+    while True:
+        for x in range(4):
+            turtle.forward(100)
+            turtle.right(90)
+        if abs(pos()) < 1:
+            break
+    end_fill()
+    color('blue')
+    begin_fill()
+    while True:
+        for y in range(3):
+            turtle.forward(100)
+            turtle.left(120)
+        if abs(pos()) < 1:
+            break
+    end_fill()
+    done()
 
-color('blue')
-begin_fill()
-while True:
-    for y in range(3):
-        turtle.forward(200)
-        turtle.left(120)
-    if abs(pos()) < 1:
-        break
-end_fill()
-done()
+draw_house()
 
 turtle.exitonclick()
