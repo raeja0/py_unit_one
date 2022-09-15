8b4e63810be37ecedaf8166651b78c2c69e49f53
raeja0 <raeja@ssfs.org>
2022-09-15 11:58:26 -0400

    Update drawing_houses.py

--- drawing_houses.py
+++ drawing_houses.py
@@ -1,27 +1,43 @@
 import turtle
 
-from turtle import *
-def draw_house():
-    color('red')
-    begin_fill()
-    while True:
-        for x in range(4):
-            turtle.forward(100)
-            turtle.right(90)
-        if abs(pos()) < 1:
-            break
-    end_fill()
-    color('blue')
-    begin_fill()
-    while True:
-        for y in range(3):
-            turtle.forward(100)
-            turtle.left(120)
-        if abs(pos()) < 1:
-            break
-    end_fill()
-    done()
+turtle.speed(8)
 
-draw_house()
+def space_house():
+    turtle.left(90)
+    turtle.penup()
+    turtle.forward(60)
+    turtle.pendown()
 
+def draw_house(house_size, house_color, roof_color):
+    turtle.fillcolor(house_color)
+    turtle.begin_fill()
+    for x in range(4):
+        turtle.forward(house_size)
+        turtle.left(90)
+    turtle.right(270)
+    turtle.forward(house_size)
+    turtle.right(90)
+    turtle.end_fill()
+    turtle.fillcolor(roof_color)
+    turtle.begin_fill()
+    for y in range(4):
+        turtle.forward(house_size)
+        turtle.left(120)
+    turtle.end_fill()
+    turtle.right(210)
+    turtle.forward(house_size)
+    #turtle.left(90)
+
+turtle.penup()
+turtle.forward(-300)
+turtle.pendown()
+
+for z in range(1):
+    draw_house(150, 'red', 'blue')
+    space_house()
+    draw_house(50, 'green', 'purple')
+    space_house()
+    draw_house(100, 'light blue', 'pink')
+    space_house()
+    draw_house(200, 'purple', 'green')
 turtle.exitonclick()
